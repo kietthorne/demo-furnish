bbfe449755e1b0a5db9d4fc1c2160ca5e466c8e6
Kietthor <phantuankiet.dev@gmail.com>
2023-03-27 20:38:16 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+
+
+
 // Lưu trữ dự liệu thông qua localstorage
 window.onload = function() {
 	if (localStorage) {
@@ -12,7 +15,27 @@ window.onload = function() {
 	}
 }
 
+function display() {
+  if (document.myForm.name.value == '')
+  return false;
+  // When it returns false - your form will not submit and will not redirect too
+else
+  return true;
+// Khi nó trả về true - biểu mẫu sẽ gửi và sẽ chuyển hướng
+// (actually it's a part of submit) id you have mentioned in action
+}
 
+
+form.addEventListener('submit', (event) => {
+  // Chặn reload trang khi submit form
+  event.preventDefault();
+})
+
+function showten() {
+  var name = document.getElementById('name').value;
+  var str = 'Hello' + ' ' + name + ' ' +'.'
+  document.getElementById('demo').innerHTML = str;
+}
 
 
 
